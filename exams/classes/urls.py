d959e422a80d3ea4601e365562cb7aec26011b3d
Ayush Kushwaha <111639899+akayushk9648@users.noreply.github.com>
2025-03-21 08:22:34 +0530

    Delete exams/classes directory

--- exams/classes/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import *
-
-urlpatterns = [
-        path('class/', ClassView.as_view(), name='class'),
-        path('subject/', SubjectView.as_view(), name='subject'),
-
-    ]
